b856ff51c8f55c49668d312c259f48fce6cde85d
Shunzhe <git@mszmagic.com>
2022-01-30 17:44:51 +0800

    Initial Commit

---
+++ SearchBarKeywordTest/SearchBarKeywordTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarKeywordTestApp.swift
+//  SearchBarKeywordTest
+//
+//  Created by Shunzhe on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarKeywordTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
